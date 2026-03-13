bcb3cbd08cfbdf0465437e3b62ad98f81827b619
Anton Zviahintsev <s31364@pjwstk.edu.pl>
2026-03-13 21:58:09 +0100

    Dodanie komunikatu bledu dla pustego wejscia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         string line = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(line)) {
+            Console.WriteLine(" BLĄD: Wejscie nie może byc puste");
+            return;
+        }
+
     }
 }
